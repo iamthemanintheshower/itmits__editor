23319aae2c6eb5683afceb972f4e227f5fde91b9
Fabio <imthemanintheshower@gmail.com>
2018-04-02 22:14:47 +0200

    Update editor-script.js

--- admin-uihsdw/public_html/js/editor-script.js
+++ admin-uihsdw/public_html/js/editor-script.js
@@ -28,10 +28,57 @@ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
 DEALINGS IN THE SOFTWARE.
 */
-
+var mouseover_div = '';
+var current_field_id = '';
 $(document).ready(function(){
     $( ".ed-cst-fld" ).attr('contenteditable','true');
     $( ".ed-cst-fld" ).attr('onblur','_saveText(this)');
+
+    $("div").contextmenu(function(e) {
+        e.preventDefault();
+        var field_id = $(this).data('field_id');
+        if(typeof field_id != "undefined" && field_id === mouseover_div){
+            current_field_id = field_id;
+            var values = {field_id: field_id};
+
+            $.post(  pluginUrl + "admin-uihsdw/public_html/index.php", values)
+            .done(function( data ) {
+                jQuery('#editor_popup').show();
+                jQuery('#text_editor').val(data.file_content);
+                scrollTextareaToText('text_editor', field_id);
+            })
+            .fail(function( data ) {
+                console.log( "FAIL: " );
+                console.log( data );
+            });
+        }
+    });
+
+    $('div').mouseover(function() {
+        var field_id = $(this).data('field_id');
+        mouseover_div = field_id;
+    });
+
+    jQuery('body').on('click', '#editor__discard_changes', function () {
+        jQuery('#editor_popup').hide();
+        jQuery('#text_editor').val('');
+    });
+
+    jQuery('body').on('click', '#editor__save', function () {
+
+        var values = {field_id: current_field_id, page_code: jQuery('#text_editor').val()};
+
+        $.post(  pluginUrl + "admin-uihsdw/public_html/index.php", values)
+        .done(function( data ) {
+            console.log(data);
+            jQuery('#editor_popup').hide();
+            jQuery('#text_editor').val('');
+        })
+        .fail(function( data ) {
+            console.log( "FAIL: " );
+            console.log( data );
+        });
+    });
 });
 
 function _saveText(obj_this){
@@ -49,3 +96,19 @@ function _saveText(obj_this){
         });
     }
 }
+
+function scrollTextareaToText(text_editor, text) {
+    var textarea = document.getElementById(text_editor);
+    var text_position = jQuery('#' + text_editor).val().indexOf(text);
+
+    textarea.focus();
+    textarea.selectionStart = text_position;
+    textarea.selectionEnd = text_position + text.length;
+
+    var line_height = jQuery('#' + text_editor).css('line-height').replace('px', '');
+    var lines_count = textarea.scrollHeight / line_height;
+    var chars_in_line = jQuery('#' + text_editor).val().length / lines_count;
+    var line_number = Math.floor(text_position / chars_in_line);
+
+    jQuery('#' + text_editor).scrollTop( line_number * line_height );
+};
